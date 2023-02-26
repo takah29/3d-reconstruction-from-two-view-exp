dcdec91f84041c77a61a952a6c402bd1dd187341
takah29 <takah0209@gmail.com>
2023-02-27 02:09:58 +0900

    Z軸を奥行きとする軸でプロットするようにした

--- lib/visualization.py
+++ lib/visualization.py
@@ -3,40 +3,46 @@ from numpy.typing import NDArray
 
 
 def init_3d_ax():
+    """Zを奥行きとした右手系座標を設定する"""
     plt.figure(figsize=(16, 16))
     ax = plt.axes(projection="3d")
 
     ax.set_xlim3d([-5.0, 5.0])
-    ax.set_xlabel("X")
+    ax.set_xlabel("Y")
 
     ax.set_ylim3d([-5.0, 5.0])
-    ax.set_ylabel("Y")
+    ax.set_ylabel("Z")
 
     ax.set_zlim3d([-5.0, 5.0])
-    ax.set_zlabel("Z")
+    ax.set_zlabel("X")
 
     return ax
 
 
-def plot_basis(pos: NDArray, basis: NDArray, ax, label=None) -> None:
+
+def plot_3d_basis(pos: NDArray, basis: NDArray, ax, label=None) -> None:
     """
     基底をプロットする。回転行列のプロットにも使用できる。
     """
     assert pos.shape == (3,)
     assert basis.shape == (3, 3)
 
+
     cols = ["r", "g", "b", "r", "r", "g", "g", "b", "b"]
     _ = ax.quiver(
-        [pos[0]] * 3, [pos[1]] * 3, [pos[2]] * 3, basis[:, 0], basis[:, 1], basis[:, 2], colors=cols
+        [pos[1]] * 3, [pos[2]] * 3, [pos[0]] * 3, basis[:, 1], basis[:, 2], basis[:, 0], colors=cols
     )
 
     if label is not None:
         ax.text(pos[0], pos[1], pos[2] + 1.0, label)
 
 
-def plot_points(points: NDArray, ax, color="black") -> None:
-    ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=color, marker="o")
+def plot_3d_points(X: NDArray, ax, color="black") -> None:
+    ax.scatter(X[:, 1], X[:, 2], X[:, 0], c=color, marker="o")
 
+
+def plot_2d_points(x, ax, color="black") -> None:
+    ax.scatter(x[:, 1], x[:, 0], c=color, marker="o")
 
 if __name__ == "__main__":
     import numpy as np
@@ -49,9 +55,9 @@ if __name__ == "__main__":
     basis = np.array(
         [[1, 0, 0], [0, np.cos(omega), -np.sin(omega)], [0, np.sin(omega), np.cos(omega)]]
     ).T  # 横ベクトル向けに転置する
-    plot_basis(pos, basis, ax, label="test")
+    plot_3dbasis(pos, basis, ax, label="test")
 
     # データ点のプロット
     X = np.eye(3) @ basis + pos
-    plot_points(X, ax, "blue")
+    plot_3d_points(X, ax, "blue")
     plt.show()
