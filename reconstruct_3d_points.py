20a88aac16b66392af07704ce11ae7d21a729745
takah29 <takah0209@gmail.com>
2023-03-12 01:40:17 +0900

    修正

--- reconstruct_3d_points.py
+++ reconstruct_3d_points.py
@@ -2,13 +2,14 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
+from lib.fundamental_matrix import calc_fundamental_matrix_8points_method, remove_outliers
 from lib.epipolar_geometry import (
     calc_focal_length,
-    calc_fundamental_matrix_8points_method,
     calc_motion_parameters,
     detect_corresponding_points,
     calc_camera_matrix,
     reconstruct_3d_points,
+    convert_image_coord_to_screen_coord
 )
 from lib.visualization import init_3d_ax, plot_3d_basis, plot_3d_points
 
@@ -21,11 +22,21 @@ def main():
     # 対応点の検出
     x1, x2 = detect_corresponding_points(img1, img2)
 
+    # 画像座標をスクリーン座標へ変換
+    x1 = convert_image_coord_to_screen_coord(x1)
+    x2 = convert_image_coord_to_screen_coord(x2)
+
+    f0 = max(img1.shape)
+
+    # アウトライアの除去
+    print(x1.shape)
+    x1, x2, _ = remove_outliers(x1, x2, f0, 2)
+    print(x1.shape)
+
     # 基礎行列Fの計算
-    F = calc_fundamental_matrix_8points_method(x1, x2)
+    F = calc_fundamental_matrix_8points_method(x1, x2, f0, normalize=True, optimal=True)
 
     # 焦点距離f, f_primeの計算
-    f0 = 1.0
     f, f_prime = calc_focal_length(F, f0)
 
     # 運動パラメータの計算
@@ -41,7 +52,6 @@ def main():
     X_ = reconstruct_3d_points(x1, x2, P, P_prime, f0)
 
     ax = init_3d_ax()
-    plot_3d_basis(np.zeros(3), np.eye(3), ax)
     plot_3d_basis(t, R, ax)
     plot_3d_points(X_, ax, "blue")
 
