74725a93579b66139b91841b708b96baaa60e5de
takah29 <takah0209@gmail.com>
2023-03-13 00:38:05 +0900

    Cameraクラスを別ファイルに移動

--- sfm_test.py
+++ sfm_test.py
@@ -1,57 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from lib.camera import Camera
+from lib.epipolar_geometry import (
+    calc_camera_matrix,
+    calc_epipole,
+    calc_fixed_focal_length,
+    calc_free_focal_length,
+    calc_motion_parameters,
+    reconstruct_3d_points,
+)
 from lib.fundamental_matrix import (
     calc_fundamental_matrix_8points_method,
-    calc_fundamental_matrix_taubin_method,
     calc_fundamental_matrix_extended_fns_method,
+    calc_fundamental_matrix_taubin_method,
     optimize_corresponding_points,
     remove_outliers,
 )
-from lib.epipolar_geometry import (
-    calc_free_focal_length,
-    calc_fixed_focal_length,
-    calc_motion_parameters,
-    calc_camera_matrix,
-    reconstruct_3d_points,
-    calc_epipole,
-)
-from lib.utils import unit_vec
-from lib.visualization import init_3d_ax, plot_3d_basis, plot_3d_points, plot_2d_points
-
-
-class Camera:
-    def __init__(self, origin=(0.0, 0.0, 0.0), target=(0.0, 0.0, 1.0), f=1.0):
-        origin = np.asarray(origin)
-        target = np.asarray(target)
-        self.o = origin
-        self.d = unit_vec(target - origin)
-        self.f = f
-
-    def get_camera_matrix(self, f0):
-        return self._calc_camera_matrix(f0)
-
-    def get_pose(self):
-        return self._calc_pose()
-
-    def _calc_camera_matrix(self, f0):
-        R, t = self._calc_pose()
-        return np.diag((self.f, self.f, f0)) @ np.hstack([R.T, -R.T @ t[:, np.newaxis]])
-
-    def _calc_pose(self):
-        world_top = np.array([1.0, 0.0, 0.0])
-        camera_z = self.d
-        camera_y = np.cross(camera_z, world_top)  # camera right
-        camera_x = np.cross(camera_y, camera_z)  # camera up
-        R = np.vstack((camera_x, camera_y, camera_z)).T
-        t = self.o
-        return R, t
-
-    def project_points(self, X, f0):
-        X_ext = np.hstack((X, np.ones((X.shape[0], 1))))
-        # print(self.get_camera_matrix(f0))
-        Xproj = X_ext @ self.get_camera_matrix(f0).T
-        return Xproj[:, :2] / Xproj[:, -1:]
+from lib.visualization import init_3d_ax, plot_2d_points, plot_3d_basis, plot_3d_points
 
 
 def set_points():
